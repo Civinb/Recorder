a2ea820461bedfdd2007503ec3695d5c31e54a83
Civinb <whiteflaghzp@gmail.com>
2026-06-01 21:58:58 -0400

    creat anime db

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import enum
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
+db = SQLAlchemy(app)
+
+
+class status(enum.Enum):
+    WACHING = "Watching"
+    COMPLETED = "Completed"
+    PLANNING = "Planning"
+    DROPPED = "Dropped"
+    ON_HOLD = "On Hold"
+    DEFAULT = "Default"
+
+
+
+class Anime(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    image_url = db.Column(db.String(200), nullable=True)
+    type = db.Column(db.String(20), nullable=False)
+    broadcast_date = db.Column(db.String(50), nullable=True)
+    summary = db.Column(db.Text, nullable=True)
+    score = db.Column(db.Float, nullable=True)
+    reviews = db.Column(db.Text, nullable=True)
+    tags = db.Column(db.String(200), nullable=True)
+    bangumi_links = db.Column(db.String(200), nullable=True)
+    official_links = db.Column(db.String(200), nullable=True)
+    status = db.Column(db.Enum(status), nullable=False, default=status.DEFAULT)
+
+with app.app_context():
+    db.create_all()
+
+'''@app.route("/power", methods=["POST"])
+def power():
+    base = request.json.get("base")
+    exponent = request.json.get("exponent")
+    print(f"Received base: {base}, exponent: {exponent}")
+    if base is None or exponent is None:
+        return jsonify({"error": "Missing 'base' or 'exponent' in request body"})
+    try:
+        result = base ** exponent
+        print(f"Calculated result: {result}")
+        return jsonify({"result": result})
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        return jsonify({"error": str(e), "message": "Please try again"})'''
+
+
+
+@app.route("/animes", methods=["POST"])
+def animes():
+    animes = Anime.query.all()
+    return render_template("anime.html", animes=animes)
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)
