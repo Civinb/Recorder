273d6350d63d4c48fc3162a68f01497dc0c634f9
Civinb <whiteflaghzp@gmail.com>
2026-06-03 00:47:14 -0400

    v2.1

--- latest_bangumi.py
+++
@@ -1,7 +0,0 @@
-import requests
-meta_url = "https://raw.githubusercontent.com/bangumi/Archive/master/aux/latest.json"
-meta = requests.get(meta_url).json()
-download_url = meta["browser_download_url"]
-time = meta["updated_at"]
-
-
